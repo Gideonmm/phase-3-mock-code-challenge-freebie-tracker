e3f262d35f952401ad9f44ee901c27e7f22dffaf
Gideon Mutugi <gideonmutugi24@gmail.com>
2023-06-04 23:43:27 +0300

    updated company

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -2,5 +2,22 @@ class Dev < ApplicationRecord
     has_many :freebies
     has_many :companies, through: :freebies
   
-    def
+    def freebies
+      Freebie.where(dev_id: id)
+    end
+  
+    def companies
+      Company.joins(:freebies).where(freebies: { dev_id: id })
+    end
+  
+    def received_one?(item_name)
+      freebies.exists?(item_name: item_name)
+    end
+  
+    def give_away(dev, freebie)
+      if freebie.dev == self
+        freebie.update(dev: dev)
+      end
+    end
+  end
   
--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,9 +1,16 @@
 class Freebie < ApplicationRecord
-    belongs_to :dev
-    belongs_to :company
-  
-    def print_details
-      "#{dev.name} owns a #{item_name} from #{company.name}"
-    end
+  belongs_to :dev
+  belongs_to :company
+
+  def dev
+    Dev.find_by(id: dev_id)
   end
-  
+
+  def company
+    Company.find_by(id: company_id)
+  end
+
+  def print_details
+    "#{dev.name} owns a #{item_name} from #{company.name}"
+  end
+end
